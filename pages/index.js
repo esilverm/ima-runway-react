2c165474c35866afe2f843b4186846f11bcf74bc
Evan Silverman <es4753@nyu.edu>
2021-11-08 14:16:25 -0500

    adjust to allow for vercel publishing

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-export default function Home({ model, image, url }) {
+export default function Home({ url }) {
+  const [image, setImage] = useState("");
+  const [model, setModel] = useState("");
+
+  useEffect(async () => {
+    const { model: modelName, image: imageUrl } = await fetch(url).then((res) =>
+      res.json()
+    );
+
+    setImage(imageUrl);
+    setModel(modelName);
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
@@ -13,21 +25,25 @@ export default function Home({ model, image, url }) {
       <main className={styles.main}>
         <h1 className={styles.title}>IMA ML</h1>
         <h3>Example of using React.js with {model} from RunwayML</h3>
-        <Image unoptimized src={image} alt={model} width={512} height={512} />
+
+        {image && (
+          <Image unoptimized src={image} alt={model} width={512} height={512} />
+        )}
       </main>
     </div>
   );
 }
 
 export async function getServerSideProps(ctx) {
-  const { model, image } = await fetch(
-    `https://ml-for-arts-runway.vercel.app/api/model`
-  ).then((res) => res.json());
+  // const { model, image } = await fetch(
+  //   `${ctx.req.headers.referer}/api/model`
+  // ).then((res) =x> res.json());
 
   return {
     props: {
-      model,
-      image,
+      // model,
+      // image,
+      url: `https://${ctx.req.headers.host}/api/model`,
     },
   };
 }
